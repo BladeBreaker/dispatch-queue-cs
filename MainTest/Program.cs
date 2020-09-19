0a61800bd0a6855a3883e5fce474196cbec3f348
Dan Liverance <daniel.liverance@gmail.com>
2020-09-19 17:17:52 -0400

    Updated project reference in Tests project Updated line endings

--- MainTest/Program.cs
+++ MainTest/Program.cs
@@ -6,19 +6,6 @@ namespace MainTest
     {
         public static void Main(string[] args)
         {
-            string null_str = null;
-            int? null_int = null;
-            int? real_int = 5;
-
-            bool test = null_str is string;          // false
-            bool test2 = null_int is int?;           // false
-            bool test3 = real_int is int?;           // true
-            bool test4 = real_int is int;            // true
-            bool test5 = real_int is Nullable<int>;  // true
-
-            Console.WriteLine("test");
-
-            /*
             Dispatch.SerialQueue queue = new Dispatch.SerialQueue(new Dispatch.ManagedThreadPool());
 
             queue.DispatchAsync(() =>
@@ -39,7 +26,6 @@ namespace MainTest
             });
 
             queue.DispatchSync(null);
-            */
         }
     }
 }
